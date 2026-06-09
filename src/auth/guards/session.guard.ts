56897d05a3e3f62367769df8599c6a5903ad809f
Emmerson Oliveira <sroliver202023@gmail.com>
2026-06-09 20:34:54 -0300

    :sparkles: feat: implements session guard

---
+++ src/auth/guards/session.guard.ts
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthService } from '../service/auth.service';
+
+@Injectable()
+export class SessionGuard implements CanActivate {
+  constructor(private readonly authService: AuthService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+
+    const sessionToken = request.headers['x-session-token'] as string;
+
+    if (!sessionToken) {
+      throw new UnauthorizedException('Session token required');
+    }
+
+    try {
+      const session = await this.authService.validateSessionToken(sessionToken);
+
+      if (!session.valid || !session.user) {
+        throw new UnauthorizedException('Invalid session token');
+      }
+
+      request.user = session.user;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid session token');
+    }
+
+    return true;
+  }
+}
