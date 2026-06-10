71565d588ffb2710206761c9dc07401a4f556817
Emmerson Oliveira <sroliver202023@gmail.com>
2026-06-10 14:14:06 -0300

    :recycle: refactor: move auth/guards to guards and implements handleRequest in ThrottlerGuard

--- src/auth/controller/auth.controller.ts
+++ src/auth/controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '../service/auth.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Throttle } from '@nestjs/throttler';
 
 @ApiTags('Authentication')
 @Controller('auth')
@@ -12,6 +13,7 @@ export class AuthController {
   @ApiOperation({ summary: 'User login' })
   @ApiResponse({ status: 200, description: 'Login successfully' })
   @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  @Throttle({ short: { limit: 5, ttl: 60000 } })
   async login(@Body() loginDto: { email: string; password: string }) {
     return this.authService.login(loginDto);
   }
@@ -21,6 +23,7 @@ export class AuthController {
   @ApiOperation({ summary: 'User registration' })
   @ApiResponse({ status: 200, description: 'Registration successfully' })
   @ApiResponse({ status: 401, description: 'Invalid registration data' })
+  @Throttle({ medium: { limit: 3, ttl: 60000 } })
   async register(@Body() registerDto: any) {
     return this.authService.register(registerDto);
   }
